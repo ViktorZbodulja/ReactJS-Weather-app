b20a193d9b777f97a51609e2b9ac83075943e421
Viktor Zbodulja <viktorzbodulja@gmail.com>
2024-04-29 21:37:22 +0200

    switching to API 3.0, optimising performances

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import "./style/app.css";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { fetchDataPicture, fetchDataCity, fetchDailyForecast, fetchAirPolution } from "./services/fetchData";
-//pics
+//Assets
 import bgImg2 from "./img/background2_img.jpg";
 import dawnImg from "./img/dawn_img.jpg";
 import dayImg from "./img/day_img.jpg";
 import eveningImg from "./img/evening_img.jpg";
 import nightImg from "./img/night_img.jpg";
-//components
+//Components
 import Main from './components/Main';
 import Footer from './components/Footer';
 import Forecast from './components/Forecast';
@@ -52,38 +52,38 @@ function App() {
       .catch(error => alert(error));
   };
  
-  let bgPicture = "";
-  let hours = new Date().getHours();
-  if (pictureData) {
-    if (pictureData.total > 0) {
-      bgPicture = pictureData.results[0].urls.regular;
-    }
-    else {
-      bgPicture = bgImg2;
-    }
-  } else {
-    if (hours > 5 && hours <= 7) {
-      bgPicture = dawnImg;
-    }
-    else if (hours > 7 && hours <= 17) {
-      bgPicture = dayImg;
-    }
-    else if (hours > 17 && hours < 20) {
-      bgPicture = eveningImg;
-    }
-    else {
-      bgPicture = nightImg;
-    }
-  }
+  useEffect(() => {
+    const hours = new Date().getHours();
+    const getBackgroundImage = async () => {
+      let bgPicture = "";
+      if (pictureData && pictureData.total > 0) {
+        bgPicture = pictureData.results[0].urls.regular;
+      } else {
+        if (hours > 5 && hours <= 7) {
+          bgPicture = dawnImg;
+        } else if (hours > 7 && hours <= 17) {
+          bgPicture = dayImg;
+        } else if (hours > 17 && hours < 20) {
+          bgPicture = eveningImg;
+        } else {
+          bgPicture = nightImg;
+        }
+      }
+      document.body.style.background = `url(${bgPicture}) no-repeat fixed center center/cover`;
+    };
+
+    getBackgroundImage();
+
+  }, [pictureData]);
 
   return (
-    <div className="App" style={{ background: `url(${bgPicture}) no-repeat fixed center center/cover` }}>
+    <div className="App">
       <div className='formClass'>
         <p>ForecastNow: Your Weather Assistant</p>
         <form className='formBlock' onSubmit={fetchData}>
           <input type="text" name="field" id="field" placeholder='Enter City' onChange={setCity} />
           <button className="searchButton" type='submit' ><AiOutlineSearch size="25px" color='white' /></button>
-          {data && data.message === "city not found" ? <div className='notExist'>City not found. Please enter other city.</div> : ""}
+          {data && data.message === "city not found" && <div className='notExist'>City not found. Please try with other city.</div>}
         </form>
       </div>
       {data && dailyData ? <Main data={data} dailyData={dailyData} changeStyle={changeStyle} button={button} airPolution={airPolution} /> : ""}
